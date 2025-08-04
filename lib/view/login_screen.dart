aa0fba1ebe04901e7614e6204dd2c23c69eda2b4
Wandile Kubheka <retroff01@gmail.com>
2025-08-04 18:15:14 +0200

    UI : basic loginscreen ui without theming

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -9,6 +9,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  
   // method to sign in user with email and password
   // get user from database then update app global state before we push replace
   Future<void> _loginButton({
@@ -23,6 +24,45 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Placeholder());
+    // keep track of email and password field state
+    final TextEditingController email  = TextEditingController();
+    final TextEditingController password  = TextEditingController();
+    // know if password is shown
+    bool isSecure = true;
+    IconData showPasswordIcon = isSecure? Icons.remove_red_eye : Icons.remove_red_eye_rounded;
+    return Scaffold(body: 
+    Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        spacing: 10,
+        mainAxisAlignment: MainAxisAlignment.center,
+        
+        children: [
+          Center(
+          child: Expanded(child: 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(decoration: InputDecoration(label: Text("Email"))),
+              TextField(decoration: InputDecoration(label: Text("Password"),suffixIcon:  GestureDetector(
+                
+                onTap: () {
+                  setState(() {
+                    isSecure = !isSecure;
+                  });
+                },
+                child: Icon(showPasswordIcon ),)),obscureText: true,),
+      
+            ],
+          )),
+          ),
+                      TextButton(onPressed: ()async{
+                await _loginButton(email: email.text, password: password.text);
+              }, child: const Text("LOGIN"))
+        ],
+      ),
+    )
+    
+    );
   }
 }
